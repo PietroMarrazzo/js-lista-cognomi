d23613a111ccf2191c9c1e0d3342f59458ead931
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-19 14:19:44 +0200

    cycle surname do while

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 // database cognomi
 var database = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
-console.log(database);
+// console.log(database);
+
+// richiesta cognome utente
+do {
+    var cognome = prompt('Inserisci il tuo cognome');
+} while (isNaN(cognome));
+
