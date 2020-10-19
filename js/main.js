9b96e7f50efd65fd0210262aba0417e326da874f
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-19 16:09:50 +0200

    trying to fix DO WHILE loop

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // database cognomi
 var database = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
 // console.log(database);
-var avaliable = 0;
+var cycle = 0;
 // richiesta cognome utente
-var cognome = 0;
+var cognome = '';
+
 do {
     var cognome = prompt('Inserisci il tuo cognome').trim();
 
-    // console.log(typeof cognome);
-    // if (isNaN(cognome)) {
-    //     var avaliable = 1;
-    // } else {
-    //     var avaliable = 0;
-    // }
-
-} while (typeof cognome !== 'string');
+    if (isNaN(cognome)) {
+        var cycle = 1;
+    } else {
+        var cycle = 0;
+    }
+} while (cycle = 0);
+// } while (typeof cognome !== 'string');
 
 // uppercase first letter if not 
 cognome = cognome.substring(0,1).toUpperCase() + cognome.substring(1).toLowerCase();
@@ -27,19 +27,19 @@ database.sort();
 
 // cycle while to create UL
 var surnames = '';
-
 for (var i = 0; i < database.length; i++) {
     var surname = database[i];
 
     surnames += '<li>' + surname + '</li>';
 }
 
+
 // show UL
 document.getElementById('list').innerHTML = surnames;
 
-
 // surname stamp
 document.getElementById("surname").innerHTML = 'Buongiorno ' + cognome;
+
 // position surname
 position = database.indexOf(cognome) + 1; 
 document.getElementById("position").innerHTML = 'Occupi la posizione numero ' + position;
