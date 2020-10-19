318081f236354f0e78470160367aeaea6f556529
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-19 14:44:18 +0200

    cycle surname wip

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 // database cognomi
 var database = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
 // console.log(database);
-
+var avaliable = 0;
 // richiesta cognome utente
+var cognome = 0;
 do {
-    var cognome = prompt('Inserisci il tuo cognome');
-} while (isNaN(cognome));
+    var cognome = prompt('Inserisci il tuo cognome').trim();
 
+    // if (isNaN(cognome)) {
+    //     var avaliable = 1;
+    // } else {
+    //     var avaliable = 0;
+    // }
+
+// } while (avaliable = 0);
+} while (typeof cognome != 'string');
+
+database.push(cognome);
+
+console.log(database);
