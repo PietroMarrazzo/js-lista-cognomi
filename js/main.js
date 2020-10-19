69df322b9c98ef28a6d8b0e1311c168a5665590b
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-19 15:05:04 +0200

    displaying results wip

--- js/main.js
+++ js/main.js
@@ -7,15 +7,24 @@ var cognome = 0;
 do {
     var cognome = prompt('Inserisci il tuo cognome').trim();
 
+    // console.log(typeof cognome);
     // if (isNaN(cognome)) {
     //     var avaliable = 1;
     // } else {
     //     var avaliable = 0;
     // }
 
-// } while (avaliable = 0);
-} while (typeof cognome != 'string');
+} while (typeof cognome !== 'string');
 
+
+//  surname stamp
+document.getElementById("surname").innerHTML = 'Buongiorno ' + cognome;
+
+// push cognome in to database array
 database.push(cognome);
 
-console.log(database);
+// database alphabetical sort
+database.sort();
+
+document.getElementById("list-surname").innerHTML = database;
+
